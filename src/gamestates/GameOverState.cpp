222fc2916f406a2b9049e4a8435d62199070faa1
meckya <amecky@gmail.com>
2015-11-12 21:10:59 +0100

    getting close to beta version

--- src/gamestates/GameOverState.cpp
+++ src/gamestates/GameOverState.cpp
@@ -52,10 +52,7 @@ int GameOverState::onGUIButton(ds::DialogID dlgID, int button) {
 // render
 // --------------------------------------------
 void GameOverState::render() {
-	//ds::sprites::draw(v2(256, 192), ds::math::buildTexture(0.0f, 512.0f, 512.0f, 384.0f));
-	//ds::sprites::draw(v2(768, 192), ds::math::buildTexture(0.0f, 512.0f, 512.0f, 384.0f));
-	//ds::sprites::draw(v2(256, 576), ds::math::buildTexture(0.0f, 512.0f, 512.0f, 384.0f));
-	//ds::sprites::draw(v2(768, 576), ds::math::buildTexture(0.0f, 512.0f, 512.0f, 384.0f));
+	ds::sprites::draw(v2(512, 384), ds::math::buildTexture(0.0f, 512.0f, 512.0f, 384.0f), 0.0f, 2.0f, 2.0f);
 }
 
 
